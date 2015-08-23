674ef0174263f8a5b1b626610550d7c17f776e71
LuizAugusto <luizaugusto_guga@hotmail.com>
2015-08-23 15:46:55 -0300

    commit anexo

--- sce-web-ref2/src/main/java/br/sceweb/controle/ConsultarAtcomp.java
+++ sce-web-ref2/src/main/java/br/sceweb/controle/ConsultarAtcomp.java
@@ -1,7 +1,14 @@
 package br.sceweb.controle;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +17,8 @@ import br.sceweb.dominio.AtcompRepositorio;
 
 public class ConsultarAtcomp implements IComando {
 	AtcompRepositorio atcompRepositorio;
+	
+	
 /**
  * O comando mostratodasempresas seleciona o sgbd desejado
  * HIBERNATE = 1
@@ -20,10 +29,21 @@ public class ConsultarAtcomp implements IComando {
 		atcompRepositorio = new AtcompRepositorio(1);
 		Atcomp a = new Atcomp();
 		a.setCodigo(Integer.parseInt(request.getParameter("codigoRadio")));
-		Atcomp atcomp = atcompRepositorio.Consultar(a);
-		request.setAttribute("atcomp", atcomp);		
+		Atcomp atcomp = atcompRepositorio.Consultar(a);	
+		
+		//BufferedImage img = ImageIO.read(new ByteArrayInputStream(atcomp.getAnexo()));
+		//ImageIO.write(img.ImagemGrafico(), "jpg", response.getOutputStream());
+		
+		//below is the different part
+        File someFile = new File("java2.pdf");
+        FileOutputStream fos = new FileOutputStream(someFile);
+        fos.write(atcomp.getAnexo());
+        fos.flush();
+        fos.close();
+		
+		request.setAttribute("atcomp", atcomp);	
 		return "/visao/TelasTCCv4/TelaConsultarAtcompAluno.jsp";
 	}
-
-
+	
+	
 }
